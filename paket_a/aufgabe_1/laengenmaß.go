8c0932b9615f17e619dbb1e0bd3318ababc9b547
Arne Witteler <witteler@arnenoah.de>
2025-04-29 16:18:05 +0200

    initial commit taskpackage_a, task 1

---
+++ paket_a/aufgabe_1/laengenmaß.go
@@ -0,0 +1,6 @@
+// Schreibe ein Programm, das eine Länge in Metern einliest und diese umrechnet in:
+// • mm
+// • km
+// • Zoll (1 Zoll = 2.54 cm)
+// • Seemeilen (1 sm = 1852 m)
+// • Lichtjahre (1 Lj = 9 460 730 472 580 800 m);
