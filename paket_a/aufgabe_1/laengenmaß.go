226284b6fb86e2bba3e9cce3ab23e867971621a0
Arne Witteler <witteler@arnenoah.de>
2025-04-30 11:25:18 +0200

    add solution for task 1

--- paket_a/aufgabe_1/laengenmaß.go
+++ paket_a/aufgabe_1/laengenmaß.go
@@ -4,3 +4,33 @@
 // • Zoll (1 Zoll = 2.54 cm)
 // • Seemeilen (1 sm = 1852 m)
 // • Lichtjahre (1 Lj = 9 460 730 472 580 800 m)
+
+package main
+
+import "fmt"
+
+func main() {
+
+	var meter int
+	var mm float64
+	var km float64
+	var zoll float64
+	var sm float64
+	var lj float64
+
+	fmt.Println("Gib eine Länge (in Metern) ein: ")
+		fmt.Scan(&meter)
+
+	mm = float64(meter) * 1000
+	km = float64(meter) / 1000
+	zoll = float64(meter) * 39.37
+	sm = float64(meter) / 1852
+	lj = float64(meter) / 9.461e+15
+
+	fmt.Println(meter, "Meter entsprechen:")
+	fmt.Println(mm, "mm")
+	fmt.Println(km, "km")
+	fmt.Println(zoll, "Zoll")
+	fmt.Println(sm, "sm")
+	fmt.Println(lj, "Lj")
+}
