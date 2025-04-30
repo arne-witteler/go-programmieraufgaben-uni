c8bdcb96b07914552818be8699958a756dd51134
Arne Witteler <witteler@arnenoah.de>
2025-04-30 11:34:42 +0200

    initial commit task 2

---
+++ paket_a/aufgabe_2/quader.go
@@ -0,0 +1,9 @@
+// Schreiben Sie ein Programm, daß die Längen der drei Seiten eines Quaders einliest
+// und folgende Größen dieses Quaders ausgibt:
+// • Volumen
+// • Kantensumme
+// • Oberfläche
+// • Umkugelradius
+// • Raumdiagonale
+
+package main
