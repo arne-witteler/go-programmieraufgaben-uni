e65d684e1b313e8f7bd2e2618d17683550dda65c
Arne Witteler <witteler@arnenoah.de>
2025-05-06 17:11:06 +0200

    add solution for task 4

--- paket_a/aufgabe_4/istRechtwinklig.go
+++ paket_a/aufgabe_4/istRechtwinklig.go
@@ -3,6 +3,9 @@ package main
 // istRechtwinklig prüft, ob das durch die Seitenlängen x, y, z gebildete
 // Dreieck rechtwinklig ist.
 func istRechtwinklig(x, y, z int) bool {
-	// bitte korrigieren Sie den Rumpf dieser Funktion.
-	return true
+	if (x*x)+(y*y) == (z*z) || (y*y)+(z*z) == (x*x) || (x*x)+(z*z) == (y*y) {
+		// bitte korrigieren Sie den Rumpf dieser Funktion.
+		return true
+	}
+	return false
 }
